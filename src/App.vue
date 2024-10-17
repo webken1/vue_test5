795831477919acf42bb364f80e2a6589b89a15fc
“webken1” <190038279@qq.com>
2024-10-17 14:26:47 +0800

    feat: addH1

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-    <h1>This is Vue3.0</h1>
+    <h1>This is Vue3.2.0</h1>
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
